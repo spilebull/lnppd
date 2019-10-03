c7c9bb408117238c1a04c2d4dda60c42dfce3419
Koyama Tetsuhiro <spilebull@gmail.com>
2019-10-03 16:34:29 +0900

    Urlパス定義

--- app/src/config/urls.py
+++ app/src/config/urls.py
@@ -2,8 +2,9 @@
 # URL Configuration
 # ==================================================
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', include('main.python.urls')),
     path('admin/', admin.site.urls),
 ]
